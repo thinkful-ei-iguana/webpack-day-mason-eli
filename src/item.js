5cfa2f7b653d8dac3d3f80ccb6e0a0c0c8ec1c4b
jpegzilla <eris@jpegzilla.com>
2019-10-31 14:23:08 -0400

    added cuid package

--- src/item.js
+++ src/item.js
@@ -1,5 +1,7 @@
+import cuid from "cuid";
+
 const validateName = function(name) {
-  if (!name) throw new TypeError('Name must not be blank');
+  if (!name) throw new TypeError("Name must not be blank");
 };
 
 const create = function(name) {
